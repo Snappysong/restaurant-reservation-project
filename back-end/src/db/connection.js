ca94a6eb41d000b516bdf395bf0c6de95b41a54e
Andrew Song <snappysong@gmail.com>
2021-07-20 19:49:09 -0400

    still trying to run tests. Installed Postgresql

--- back-end/src/db/connection.js
+++ back-end/src/db/connection.js
@@ -1,4 +1,5 @@
 const environment = process.env.NODE_ENV || "development";
+console.log(environment);
 const config = require("../../knexfile")[environment];
 const knex = require("knex")(config);
 
