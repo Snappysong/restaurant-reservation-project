f839ed46a50e677d37b0ffa9662f77e040e394cf
Andrew Song <snappysong@gmail.com>
2021-08-05 13:35:04 -0400

    testing backend connection

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -3,6 +3,10 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import { BrowserRouter as Router } from "react-router-dom";
 
+import dotenv from 'dotenv'
+dotenv.config()
+
+
 ReactDOM.render(
   <React.StrictMode>
     <Router>
